0f20f47a27f322ad3b0ed495dfe60690e4d32ad0
seyha30 <sinseyha1x@github.com>
2023-04-21 11:59:09 +0700

    123

--- src/modalContext.js
+++ src/modalContext.js
@@ -2,9 +2,6 @@ import React from "react";
 import useModal from "./useModal";
 import Modal from "./modal";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 let ModalContext;
 let { Provider } = (ModalContext = React.createContext());
 
